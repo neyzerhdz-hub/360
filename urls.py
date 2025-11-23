102a582804696919cea3083ece7088665058e1fd
neyzerhdz-hub <neyzerhdz@gmail.com>
2025-11-23 02:11:19 -0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,30 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for mi_proyecto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_root(request):
+    return redirect('login')  # o 'home'
 
 urlpatterns = [
-    path('registro/', views.registro, name='registro'),
-    path('login/', views.login_view, name='login'),
-    path('home/', views.home, name='home'),
-    path('logout/', views.logout_view, name='logout'),
+    path('', redirect_root),  # <- raíz redirige a login
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls')),
+    
 ]
+
